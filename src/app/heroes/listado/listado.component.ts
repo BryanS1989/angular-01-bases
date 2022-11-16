5806cd5fdb5e0872efab80d528211ae345057621
Bryan <bryanstevenal@gmail.com>
2022-11-16 20:15:47 +0100

    Create listado component and ngFor directive

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+})
+export class ListadoComponent /*implements OnInit*/ {
+  /*
+  constructor() {
+    console.log("[app-listado] Constructor()");
+  }
+
+  ngOnInit(): void {
+    console.log("[app-listado] ngOnInit()");
+  }
+  */
+
+  public  heroes        : string[]  = [
+    'Spiderman', 'Ironman', 'Hulk',
+    'Thor', 'Capitán América'
+  ];
+  public  deletedHeroe  : string    = '';
+
+  borrarHeroe () {
+    console.log("[app-listado] borrarHeroe()");
+
+    this.deletedHeroe = this.heroes.pop() || "No hay más héroes!!!";
+    console.log("[app-listado]    Héroe borrado: " + this.deletedHeroe);
+
+  }
+
+}
