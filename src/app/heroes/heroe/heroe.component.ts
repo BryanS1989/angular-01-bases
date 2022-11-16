0c15838ab7dca35030d50d5267a5fbb89aee07df
Bryan <bryanstevenal@gmail.com>
2022-11-16 19:36:08 +0100

    Create heroe component

---
+++ src/app/heroes/heroe/heroe.component.ts
@@ -0,0 +1,47 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector      : 'app-heroe',
+  templateUrl   : './heroe.component.html'
+})
+export class HeroeComponent {
+
+  public  nombre  : string  = 'Iron Man';
+  public  edad    : number  = 45;
+
+  /*
+   * GETTERS $ SETTERS
+   * =================
+   */
+
+  /**
+   * Retorna el nombre de un heroe capitalizado
+   */
+  get nombreCapitalizado() : string {
+    return this.nombre.toUpperCase();
+  }
+
+  /*
+   * FUNCIONES
+   * =========
+   */
+
+  /**
+   * Retorna Nombre y edad concatenado
+   * @returns string
+   */
+  obtenerNombre() : string {
+    return `${ this.nombre } - ${ this.edad }`;
+    // return this.nombre + ' - ' + this.edad.toString();
+  }
+
+  cambiarNombre() : void {
+    console.log('cambiarNombre()');
+    this.nombre = 'Spiderman';
+  }
+
+  cambiarEdad() : void {
+    console.log('cambiarEdad()');
+    this.edad = 30;
+  }
+}
