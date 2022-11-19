994aff4538828ad1750eaa81763bf9d2c2f3c117
Bryan <bryanstevenal@gmail.com>
2022-11-19 19:00:50 +0100

    Create HeroesModule

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { HeroeComponent } from "./heroe/heroe.component";
+import { ListadoComponent } from "./listado/listado.component";
+
+@NgModule({
+  // Componentes que componen el módulo
+  declarations : [
+    HeroeComponent,
+    ListadoComponent
+  ],
+
+  // Cosas públicas/visibles para fuera del módulo
+  exports : [
+    HeroeComponent,
+    ListadoComponent
+  ],
+
+  // Módulos que se importarán
+  imports : [
+    CommonModule
+  ]
+})
+export class HeroesModule {
+
+}
