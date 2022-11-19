8e893ba255409a87489a0696437750129912d70b
Bryan <bryanstevenal@gmail.com>
2022-11-19 21:46:50 +0100

    Create new personajes component

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -8,11 +8,13 @@ import { FormsModule } from '@angular/forms';
 
 // Importaciones propias
 import { MainPageComponent } from './main-page/main-page.component';
+import { PersonajesComponent } from './personajes/personajes.component';
 
 
 @NgModule({
   declarations: [
-    MainPageComponent
+    MainPageComponent,
+    PersonajesComponent
   ],
   imports: [
     CommonModule,
